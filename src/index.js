4369ef4fb1af848802a8bae29a2aaeba0185a3ca
Harshit Duggal <harshitduggal101@gmail.com>
2025-08-15 15:15:53 +0530

    Added Custom Error , sucess response & added async handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import connectDb from "./db/index.js";
 import { configDotenv } from "dotenv";
+import { app } from "./app.js";
 
 configDotenv({path:"../.env"})
 
-connectDb();
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 3000 , () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+    app.on("error",(error) => {
+            console.log(`Error on connectDB ${error}`);
+            throw error;
+        })
+})
+.catch((error) => {
+    console.log("Mongo DB server not connected", error)
+})
